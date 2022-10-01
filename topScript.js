0663386204c24f2af1430db902d10f1f2cad9bc6
JoshTrono <josh.trono@gmail.com>
2022-09-30 22:23:05 -0400

    t

---
+++ topScript.js
@@ -0,0 +1,7 @@
+topButton = document.getElementById("myBtn");
+
+
+function toTopFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; //for the other browsers
+  }
